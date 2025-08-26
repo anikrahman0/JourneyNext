9c06ad39ede8ef53a88cb300e088671440974a92
Md. Anik Rahman <a7604366@gmail.com>
2025-08-26 10:42:40 +0600

    generated metadata

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -2,6 +2,18 @@ import getPost from '@/lib/getPost';
 import Link from 'next/link';
 import React from 'react';
 
+
+export async function generateMetadata({ params }) {
+    const { id } = await params;
+    const post = await getPost(id);
+
+    return {
+        title: post.title,
+        description: post.body,
+    }
+}
+
+
 export default async function PostPage({params}) {
     const {id} = await params;
     const post = await getPost(id);
